81f39aa8bc52f8a367585fa9e711cbbeb8195199
freelancer-Rafin <79695553+freelancer-Rafin@users.noreply.github.com>
2021-02-26 16:33:32 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   
+
+  $('.video-popup').magnificPopup({
+    type: 'iframe'
+    // other options
+  });   
+
+ // other options
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+});
